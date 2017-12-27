159065a119ae0aaf320460e0960f09b6a4acca50
Vixar Vorlavongc <vixar339@gmail.com>
2017-12-27 10:31:04 +0700

    updated app.js file

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var port = 8080;
+
+app.get('/', (req, res) => {
+	res.send('OK');
+})
+
+server.listen(port, () => {
+	console.log('server is runinig');
+});
